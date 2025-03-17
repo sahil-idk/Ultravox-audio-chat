4d3dfce6cbbb98e344b03cd8d518d0176659414c
sahil-idk <sahil.sahil@research.iiit.ac.in>
2025-03-17 17:38:57 +0530

    disconnected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -703,7 +703,7 @@ Initial greeting: ${customPrompt}`,
       return (
         <div className="flex items-center">
           <div className="w-2 h-2 bg-red-500 rounded-full mr-2"></div>
-          Click microphone to start
+          Disconnected 
         </div>
       );
     }
